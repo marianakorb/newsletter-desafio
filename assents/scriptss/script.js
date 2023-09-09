fd391890c09cf65cdf1717bc249682e68f63f34d
mariana <mariana.korb.mk@gmail.com>
2023-09-09 18:11:40 -0300

    add message valid email required

--- assents/scriptss/script.js
+++ assents/scriptss/script.js
@@ -1,16 +1,27 @@
 
-
 function validar() {
     const section = document.querySelector("section#singup");
     const divSuccess = document.querySelector(".div-success-message");
     section.style.display = "none";
     divSuccess.style.display = "block";
+
+    
 }
 
 function fecharAba() {  
     const section = document.querySelector("section#singup");
     const divSuccess = document.querySelector(".div-success-message");
-
+    
     divSuccess.style.display = "none";
     section.style.display = "flex";
+}
+
+function emailRequired() {
+
+    const input = document.querySelector("#email-box");
+    const message = document.querySelector("span.erro-message");
+    
+    message.style.display = "block";
+    input.classList.add("erro");
+
 }
